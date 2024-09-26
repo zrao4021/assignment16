c0944fbdddf3dc71bcfbfddc5dcd6c0b161db112
zrao4021 <zrao4021@gmail.com>
2024-09-26 15:11:03 +0500

    this is assignment number 16

---
+++ app.js
@@ -0,0 +1,173 @@
+// question 04
+
+let input = +prompt("Enter any number");
+let range = +prompt("Enter range");
+
+for(let i = 1; i<=range; i++){
+   document.write(`${input}*${i}=${input*i}<br>`);
+}
+// question completed....
+
+
+// question 03
+ 
+// let numericcounting = [1,2,3,4,5,6,7,8,9,10]
+
+for(let i = 1; i<=10; i++){
+     console.log(i);
+}
+// completed.....
+
+// question 5
+
+let fruits = ["Apple","Banana","Mango","Orange","Strawberry"]
+
+for(let i = 0; i<=4; i++){
+   document.write(`element at index ${i} is  ${fruits[i]} <br>`)
+}
+
+// completed....
+// question 6 
+
+document.write("<h2>counting</h2> 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,");
+document.write("<h2>Reserve Counting</h2> 10,9,8,7,6,5,4,3,2,1");
+document.write("<h2>Even</h2> 0,2,4,6,8,10,12,14,16,18,20");
+document.write("<h2>Odd</h2> 1,3,5,7,9,11,13,15,17,19");
+document.write("<h2>Series</h2> 2k,4k,6k,8k,10k,12k,14k,16k,18k,20k");
+
+// completed.....
+// question number 7
+
+// let customerinput  = prompt("what do you want to order");
+
+// let backeryitems = ["cake","apple pie","cookie","chips","patties"];
+
+//  lets it skip for now...
+
+// let arr = [12,14,45,89,15];
+
+// let max = Math.max(...arr);
+// let min = Math.min(...arr);
+
+// console.log(`max: ${max}, min:${min}`);
+
+
+// Question number 8
+
+let arr = [24,53,78,91,12];
+
+let max = Math.max(...arr);
+
+console.log(`Array Items:[24,53,78,91,12]`)
+console.log(`the largest number is = ${max}`)
+
+
+
+// completed
+
+// Question number 9
+
+let minimum = [24,53,78,91,12];
+
+let min  = Math.min(...minimum);
+
+console.log(`Array Items:[24,53,78,91,12]`)
+
+console.log(`the smallest number is = ${min}`);
+
+// completed
+
+// practice...q 1
+
+// let zohra = [1,2,3,4,5,6,7,88,9]
+
+// let name = Math.max(...zohra)
+// let sirname = Math.min(...zohra) 
+
+// console.log(`largest number = ${name}`)
+// console.log(`smallest number = ${sirname}`);
+
+// practice...q 2
+
+
+// let university = [67,45,98,77,98,12]
+
+// let number = Math.max(...university);
+// let anothernumber = Math.min(...university)
+
+// console.log(`max = ${number}, min = ${anothernumber}`);
+
+// done....
+
+// question number 10....
+
+for(let i = 5; i<=100;i+= 5){
+   console.log(i);
+}
+
+// another way.....
+// for(let i = 1; i<=100;i++){
+//    if(i % 5 === 0){
+//      console.log(i)
+//    }
+//    else{"sorry"}
+// }
+
+// practice.......
+
+// for(let i = 12; i<=100; i++){
+//    if(i % 12 === 0){
+//      console.log(i)
+//    }
+// }
+
+
+// question number 2.
+
+let matrix = [
+   ["0","1","2"],
+   ["3","1","0"],
+   ["1","2","2"],
+   ["1","0","1"]
+]
+
+// completed...
+
+// question number 1
+
+let multidimentional_array = [[],[],[],[],[],[],[],[],[],[]];
+
+// completed....
+
+// let item = prompt("enter what do you want")
+// let backery = ["apple","banana","orange"];
+// for(let i = 0; i <= 2; i++){
+//     if(i == backery){
+//         alert(`yes we have ${i}`)
+//     }
+        
+//         else{
+//         alert("sorry")
+
+//         }
+// }
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+
+
+
